cb1f18de42fe30e82dac519926fd7edc0a59e710
David Pecot <david.pecot@dctechusa.com>
2020-12-04 16:29:27 -0500

    initial commit

---
+++ DataGridTest/Data/Ship.cs
@@ -0,0 +1,9 @@
+﻿namespace DataGridTest.Data
+{
+    public class Ship
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Launched { get; set; }
+    }
+}
---
+++ DataGridTest/Pages/Index.razor.cs
@@ -0,0 +1,107 @@
+﻿using DataGridTest.Data;
+using Radzen.Blazor;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace DataGridTest.Pages
+{
+    public partial class Index
+    {
+
+
+
+        private RadzenGrid<Ship> DataGrid;
+        private IEnumerable<Ship> hobbits;
+        private IList<Ship> DDL_Data;
+
+        protected override void OnInitialized()
+        {
+            var data = new List<Ship>();
+            data.Add(new Ship { Id = 1, Name = "Cutty Sark", Launched = 1869 } );
+            data.Add(new Ship { Id = 2, Name = "Edmund Fitzgerald", Launched = 1958 } );
+            data.Add(new Ship { Id = 3, Name = "Kon Tiki", Launched = 1947 } );
+            hobbits = data;
+
+            DDL_Data = new List<Ship>
+            {
+                new Ship { Id = 1, Name = "Cutty Sark", Launched = 1869 },
+                new Ship { Id = 2, Name = "Edmund Fitzgerald", Launched = 1958 },
+                new Ship { Id = 3, Name = "Kon Tiki", Launched = 1947 },
+                new Ship { Id = 1, Name = "Mary Celeste", Launched = 1860 },
+                new Ship { Id = 2, Name = "Pequod", Launched = 1840 },
+                new Ship { Id = 3, Name = "Queen Anne's Revenge", Launched = 1710 },
+                new Ship { Id = 4, Name = "RMS Titanic", Launched = 1911 },
+                new Ship { Id = 5, Name = "Trieste", Launched = 1953 }
+            };
+
+
+        }
+
+        protected void Refresh()
+        {
+            Debug.WriteLine("~~Refresh~~");
+
+        }
+
+        protected void InsertRow()
+        {
+            print("InsertRow-Before");
+            var newRecipe = new Ship();
+            DataGrid.InsertRow(newRecipe).Wait();
+            // await DataGrid.UpdateRow(newRecipe);
+            Save(newRecipe);
+            //Foo(true);
+            print("InsertRow-After");
+
+        }
+
+        void OnCreateRow(Ship hobbit) { print(); }
+
+        void OnUpdateRow(Ship hobbit) { print(); }
+
+        void OnEditRow(Ship hobbit) { print(withCounts: false); }
+
+        void Save(Ship hobbit)
+        {
+            print("beginning save...", false, false);
+            print();
+            DataGrid.UpdateRow(hobbit).Wait();
+            //hobbits = hobbits.Append(hobbit);
+            print();
+
+        }
+
+        //DEBUG
+        private void print([System.Runtime.CompilerServices.CallerMemberName] string name = "", bool withCounts = true, bool withSquiggles = true)
+        {
+            string msg = "~~" + name;
+            if (withCounts)
+                msg += $"({hobbits.Count()},{DataGrid.Data.Count()})";
+            msg += "~~";
+            Debug.WriteLine(msg);
+        }
+
+        //DEBUG
+        protected void Foo(bool detailed = false)
+        {
+            print();
+            if (detailed)
+            {
+                Debug.WriteLine("hobbit data---------");
+                foreach (var item in hobbits)
+                {
+                    Debug.WriteLine(item.Name + " " + item.Launched);
+                }
+                Debug.WriteLine("grid data---------");
+                foreach (var item in hobbits)
+                {
+                    Debug.WriteLine(item.Name + " " + item.Launched);
+                }
+            }
+        }
+
+    }
+
+
+}
