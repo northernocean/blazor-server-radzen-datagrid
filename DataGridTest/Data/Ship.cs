ff5c9ae4130ab0f0168419669a300d701b97fb61
David Pecot <david.pecot@dctechusa.com>
2020-12-07 10:36:21 -0500

    CRUD Operations

--- DataGridTest/Data/Ship.cs
+++ DataGridTest/Data/Ship.cs
@@ -3,7 +3,18 @@
     public class Ship
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = "-";
         public int Launched { get; set; }
+
+        public void Update(Ship ship)
+        {
+            Name = ship.Name;
+            Launched = ship.Launched;
+        }
+
+        public Ship Clone()
+        {
+            return (Ship)MemberwiseClone();
+        }
     }
 }
--- DataGridTest/Pages/Index.razor.cs
+++ DataGridTest/Pages/Index.razor.cs
@@ -1,5 +1,8 @@
 ﻿using DataGridTest.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using Radzen.Blazor;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -9,113 +12,208 @@ namespace DataGridTest.Pages
     public partial class Index
     {
 
+        [Inject]
+        IJSRuntime JS { get; set; }
 
 
         private RadzenGrid<Ship> DataGrid;
-        private IEnumerable<Ship> ships;
-        private IList<Ship> DDL_Data;
-        private readonly IList<string> Messages = new List<string>();
+        private IList<Ship> shipsDB = new List<Ship>();
+        private IList<Ship> ships = new List<Ship>();
+        private IList<string> Messages = new List<string>();
         private string console_output = "";
 
         protected override void OnInitialized()
         {
-            var data_store = new List<Ship>
-            {
-                new Ship { Id = 1, Name = "Cutty Sark", Launched = 1869 },
-                new Ship { Id = 2, Name = "Edmund Fitzgerald", Launched = 1958 },
-                new Ship { Id = 3, Name = "Kon Tiki", Launched = 1947 }
-            };
-            ships = data_store;
+            base.OnInitialized();
 
-            DDL_Data = new List<Ship>
-            {
-                new Ship { Id = 1, Name = "Cutty Sark", Launched = 1869 },
-                new Ship { Id = 2, Name = "Edmund Fitzgerald", Launched = 1958 },
-                new Ship { Id = 3, Name = "Kon Tiki", Launched = 1947 },
-                new Ship { Id = 1, Name = "Mary Celeste", Launched = 1860 },
-                new Ship { Id = 2, Name = "Pequod", Launched = 1840 },
-                new Ship { Id = 3, Name = "Queen Anne's Revenge", Launched = 1710 },
-                new Ship { Id = 4, Name = "RMS Titanic", Launched = 1911 },
-                new Ship { Id = 5, Name = "Trieste", Launched = 1953 }
-            };
+            // Representation of persistent "database" data (i.e., external DB)
+            PopulateShipsDB();
 
+            // Representation of transient local data (i.e., queried/retrieved from DB)
+            foreach(var ship in shipsDB)
+            {
+                ships.Add(ship.Clone());
+            }
 
         }
 
-        protected void Refresh()
+        void EditRow(Ship ship)
         {
-            Debug.WriteLine("~~Refresh~~");
+            // ---------------------------------------
+            // Blazor Grid Demo: grid.EditRow(context);
 
+            DataGrid.EditRow(ship);
+        }
+
+        void OnUpdateRow(Ship ship)
+        {
+            // --------------------------------------------------
+            // Blazor Grid Demo: update dbContext, save dbContext.
+            //  eg. dbContext.Update(order);
+            //      dbContext.SaveChanges();
+            //  Also for demo purposes only sync local object with dbContext object
+
+            //Question: any reason to do this here rather than in the UpdateRow() method?
+
+            print("OnUpdateRow " + data_counts);
+        }
+
+        void SaveRow(Ship ship)
+        {
+            // -----------------------------------------
+            // Blazor Grid Demo: grid.UpdateRow(context);
+
+            print("SaveRow-Start " + data_counts);
+            DataGrid.UpdateRow(ship);
+            //ships = ships.Append(ship);
+            print("SaveRow-End " + data_counts);
+        }
+
+        void CancelEdit(Ship ship)
+        {
+            // ---------------------------------------------
+            // Blazor Grid Demo: grid.CancelEditRow(context);
+            // if modified, restore original state (uses EF tracking)
+
+            if(ship.Id > 0)
+            {
+                // Cancelling an update
+                print("CancelEdit-Start " + $"{ship.Id}/{ship.Name}/{ship.Launched}");
+                DataGrid.CancelEditRow(ship);
+                Ship shipInDB = shipsDB.Where(c => c.Id == ship.Id).SingleOrDefault();
+                if(shipInDB != null)
+                {
+                    ship.Update(shipInDB);
+                }
+                else
+                {
+                    print("Error - invalid state encountered: ShipID > 0 but ship was never saved to DB.");
+                }
+                print("CancelEdit-End " + $"{ship.Id}/{ship.Name}/{ship.Launched}");
+            }
+            else
+            {
+                // Cancelling an insert
+                print("CancelEdit-Start " + $"{ship.Id}/{ship.Name}/{ship.Launched}");
+                DataGrid.CancelEditRow(ship);
+                print("CancelEdit-End");
+
+            }
+        }
+
+        void DeleteRow(Ship ship)
+        {
+            // ----------------
+            // Blazor Grid Demo:
+            // IF ship in data_store (local) THEN
+            //   remove from data_store (local) -- ex. dbContext.Remove<T>)(t)
+            //   persist changes to underlying data_store -- ex. dbContext.SaveChanges()
+            //   DataGrid.Reload();
+            //ELSE
+            //   DataGrid.CancelEditRow(ship);
         }
 
         protected void InsertRow()
         {
-            print("InsertRow-Before");
+            // -----------------------------------------
+            // Blazor Grid Demo: grid.InsertRow(context);
+
+            print("InsertRow-Start " + data_counts);
             var newShip = new Ship();
-            DataGrid.InsertRow(newShip).Wait();
-            // await DataGrid.UpdateRow(newRecipe);
+            DataGrid.InsertRow(newShip);
+            //await DataGrid.UpdateRow(newRecipe);
             //Save(newShip);
             //Foo(true);
-            print("InsertRow-After");
+            print("InsertRow-End " + data_counts);
+        }
+
+        void OnCreateRow(Ship ship)
+        {
+            // ----------------
+            // Blazor Grid Demo:
+            //   dbContext.Add(order);
+            //   dbContext.SaveChanges();
+            //   Also for demo purposes only sync local object with dbContext object
+            //    eg: order.Customer = dbContext.Customers.Find(order.CustomerID);
+            //        order.Employee = dbContext.Employees.Find(order.EmployeeID);
+            // Note: This implies saving the row permanently as soon as a row is created.
+
+            print("OnCreateRow " + data_counts);
 
         }
 
-        void OnCreateRow(Ship ship) { print(); }
-
-        void OnUpdateRow(Ship ship) { print(); }
-
-        void OnEditRow(Ship ship) { print(withCounts: false); }
-
-        void Save(Ship ship)
+        void OnEditRow(Ship ship)
         {
-            print("beginning save...", false);
-            print();
-            DataGrid.UpdateRow(ship).Wait();
-            //ships = ships.Append(ship);
-            print();
+            // ---------------------------------------
+            // Blazor Grid Demo: no definition in demo
 
+            print("OnEditRow " + data_counts);
         }
 
         //DEBUG
-        private void MockWriteToConsole(string msg)
+        private async void MockWriteToConsole(string msg)
         {
-            Messages.Add(msg);
-            while (Messages.Count() > 30)
+            string[] lines = msg.Split(
+                new[] { "\r\n", "\r", "\n" },
+                StringSplitOptions.None);
+
+            Messages = Messages.Concat(lines).ToList();
+            while (false && Messages.Count() > 30)
             {
                 Messages.RemoveAt(0);
             }
             console_output = string.Join("\n", Messages);
             StateHasChanged();
+            await JS.InvokeVoidAsync("setMyScrollTextArea");
         }
 
-        private void print(
-            [System.Runtime.CompilerServices.CallerMemberName] string name = "", 
-            bool withCounts = true)
+        private void print(string msg)
         {
-            string msg = name;
-            if (withCounts)
-                msg += $"({ships.Count()},{DataGrid.Data.Count()})";
             MockWriteToConsole(msg);
             Debug.WriteLine(msg);
         }
 
         //DEBUG
-        protected void Foo(bool detailed = false)
+        private string Dump()
         {
-            print();
-            if (detailed)
+            string s = "";
+            s += "ship data: { ";
+            foreach (var item in ships)
             {
-                Debug.WriteLine("ship data---------");
-                foreach (var item in ships)
-                {
-                    Debug.WriteLine(item.Name + " " + item.Launched);
-                }
-                Debug.WriteLine("grid data---------");
-                foreach (var item in ships)
-                {
-                    Debug.WriteLine(item.Name + " " + item.Launched);
-                }
+                s += $"{item.Id}: {item.Name} ({item.Launched}), ";
             }
+            s = s.Substring(0, s.Length - 2);
+            s += " }\n";
+
+            s += "grid data: { ";
+            foreach (var item in DataGrid.Data)
+            {
+                s += $"{item.Id}: {item.Name} ({item.Launched}), ";
+            }
+            s = s.Substring(0, s.Length - 2);
+            s += " }";
+            return s;
+        }
+
+        protected void Refresh()
+        {
+            print(Dump());
+        }
+
+        private string data_counts => $"({shipsDB.Count()}, {ships.Count()}, {DataGrid.Data.Count()})";
+
+        private void PopulateShipsDB()
+        {
+            shipsDB.Add(new Ship { Id=1, Name="Cutty Sark", Launched=1869 });
+            shipsDB.Add(new Ship { Id=2, Name="Edmund Fitzgerald", Launched=1958 });
+            shipsDB.Add(new Ship { Id=3, Name="Kon Tiki", Launched=1947 });
+            shipsDB.Add(new Ship { Id=4, Name="Edmund Fitzgerald", Launched=1958 });
+            //shipsDB.Add(new Ship { Id=5, Name="Kon Tiki", Launched=1947 });
+            //shipsDB.Add(new Ship { Id=6, Name="Mary Celeste", Launched=1860 });
+            //shipsDB.Add(new Ship { Id=7, Name="Pequod", Launched=1840 });
+            //shipsDB.Add(new Ship { Id=8, Name="Queen Anne's Revenge", Launched=1710 });
+            //shipsDB.Add(new Ship { Id=9, Name="RMS Titanic", Launched=1911 });
+            //shipsDB.Add(new Ship { Id=10, Name="Trieste", Launched=1953 });
         }
 
     }
